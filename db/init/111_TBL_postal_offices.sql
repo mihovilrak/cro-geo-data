819edcf51c165694bcabeccff40deb01f5181f4a
mihovilrak <mihovil.1978@hotmail.com>
2025-11-07 23:40:56 +0100

    Created postal offices table

---
+++ db/init/111_TBL_postal_offices.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS postal_offices (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    geom GEOMETRY(POINT, 3765) NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_postal_offices_geom ON postal_offices USING GIST (geom);
+CREATE INDEX IF NOT EXISTS idx_postal_offices_name ON postal_offices (name);
