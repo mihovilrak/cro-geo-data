b7cd3f2617b667121f397f52682ed027f59e8a93
mihovilrak <mihovil.1978@hotmail.com>
2025-11-09 17:59:37 +0100

    Added schema and alternate_code

--- db/init/102_TBL_streets.sql
+++ db/init/102_TBL_streets.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS streets (
+CREATE TABLE IF NOT EXISTS rpj.streets (
     id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     settlement_code INT NOT NULL REFERENCES settlements(national_code),
+    alternate_code INT NOT NULL UNIQUE,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     extent GEOMETRY(POLYGON, 3765) NOT NULL
 );
-CREATE INDEX IF NOT EXISTS idx_streets_extent ON streets USING GIST (extent);
-CREATE INDEX IF NOT EXISTS idx_streets_name ON streets (name);
-CREATE INDEX IF NOT EXISTS idx_streets_settlement_code ON streets (settlement_code);
+CREATE INDEX IF NOT EXISTS idx_streets_extent ON rpj.streets USING GIST (extent);
+CREATE INDEX IF NOT EXISTS idx_streets_name ON rpj.streets (name);
+CREATE INDEX IF NOT EXISTS idx_streets_settlement_code ON rpj.streets (settlement_code);
