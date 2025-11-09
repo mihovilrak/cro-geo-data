8e65cc81c4fa7469b284cb8d910b5d716957c65e
mihovilrak <mihovil.1978@hotmail.com>
2025-11-09 18:03:06 +0100

    Added updated_at

--- backend/scripts/sql/adrese.sql
+++ backend/scripts/sql/adrese.sql
@@ -1,4 +1,5 @@
 SELECT  CAST(SUBSTR(localId, instr(localId, '.') + 1) AS INTEGER) as id,
         AlternateAddress as alternate_address,
+        beginLifespanVersion as updated_at,
         geom
 FROM Addresses;
