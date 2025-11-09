1415d75862c57565d3020a42383b1536df233af9
mihovilrak <mihovil.1978@hotmail.com>
2025-11-09 21:54:43 +0100

    Renamed to addresses, added null values for future fields

---
+++ backend/scripts/sql/addresses.sql
@@ -0,0 +1,11 @@
+SELECT CAST(SUBSTR(localId, instr(localId, '.') + 1) AS INTEGER) AS id,
+    AlternateAddress AS alternate_address,
+    NULL AS street_id,
+    NULL AS street_name,
+    NULL AS house_number,
+    NULL AS settlement_id,
+    NULL AS settlement_name,
+    NULL AS zip,
+    beginLifespanVersion AS updated_at,
+    geom
+FROM Addresses;
--- backend/scripts/sql/adrese.sql
+++
@@ -1,5 +0,0 @@
-SELECT  CAST(SUBSTR(localId, instr(localId, '.') + 1) AS INTEGER) as id,
-        AlternateAddress as alternate_address,
-        beginLifespanVersion as updated_at,
-        geom
-FROM Addresses;
