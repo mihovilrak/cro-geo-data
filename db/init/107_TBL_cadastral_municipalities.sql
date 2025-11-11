11cc1e4ef7c8b812dcbb207f909b7f6adf0efbfe
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 21:30:58 +0100

    Changed national_code to unique

--- db/init/107_TBL_cadastral_municipalities.sql
+++ db/init/107_TBL_cadastral_municipalities.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS dkp.cadastral_municipalities (
     id INT PRIMARY KEY,
-    national_code INT NOT NULL,
+    national_code INT NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     harmonization_status INT NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -8,5 +8,4 @@ CREATE TABLE IF NOT EXISTS dkp.cadastral_municipalities (
 );
 CREATE INDEX IF NOT EXISTS idx_cadastral_municipalities_geom 
 ON dkp.cadastral_municipalities USING GIST (geom);
-CREATE INDEX IF NOT EXISTS idx_cadastral_municipalities_national_code 
-ON dkp.cadastral_municipalities (national_code);
+
