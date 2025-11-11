6ca7683251ae51355e027a53c4b7bf5ad87d0975
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 18:56:44 +0100

    Created u_postal_offices

---
+++ db/init/210_UTBL_u_postal_offices.sql
@@ -0,0 +1,7 @@
+CREATE UNLOGGED TABLE IF NOT EXISTS staging.u_postal_offices (
+    id INT PRIMARY KEY,
+    postal_code VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    geom GEOMETRY(POINT, 3765) NULL
+);
