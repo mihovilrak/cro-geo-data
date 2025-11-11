0aa3c0787e7302a42f500e0261065e0d20197023
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 21:32:51 +0100

    Added postal_code

--- db/init/210_UTBL_u_postal_offices.sql
+++ db/init/210_UTBL_u_postal_offices.sql
@@ -1,6 +1,6 @@
 CREATE UNLOGGED TABLE IF NOT EXISTS staging.u_postal_offices (
     id INT PRIMARY KEY,
-    postal_code VARCHAR(255) NOT NULL,
+    postal_code INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     geom GEOMETRY(POINT, 3765) NULL
