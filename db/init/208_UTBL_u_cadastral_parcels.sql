d93c15694463b2aa1c705c32cef773538053102a
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 21:33:38 +0100

    Changed parcel_code to varchar(20), graphical_area to numeric(12, 2)

--- db/init/208_UTBL_u_cadastral_parcels.sql
+++ db/init/208_UTBL_u_cadastral_parcels.sql
@@ -1,8 +1,8 @@
 CREATE UNLOGGED TABLE IF NOT EXISTS staging.u_cadastral_parcels (
     id INT PRIMARY KEY,
-    parcel_code VARCHAR(255) NOT NULL,
+    parcel_code VARCHAR(20) NOT NULL,
     cadastral_municipality_code INT NOT NULL,
-    graphical_area FLOAT NOT NULL,
+    graphical_area NUMERIC(12, 2) NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     geom GEOMETRY(MULTIPOLYGON, 3765) NOT NULL
 );
