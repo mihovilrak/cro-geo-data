110c2c4672544ca373cbf1bf6c34c075177d2e5a
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 21:36:42 +0100

    Added schema

--- db/init/111_TBL_postal_offices.sql
+++ db/init/111_TBL_postal_offices.sql
@@ -1,9 +1,12 @@
-CREATE TABLE IF NOT EXISTS postal_offices (
+CREATE TABLE IF NOT EXISTS rpj.postal_offices (
     id INT PRIMARY KEY,
     postal_code INT NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     geom GEOMETRY(POINT, 3765) NOT NULL
 );
-CREATE INDEX IF NOT EXISTS idx_postal_offices_geom ON postal_offices USING GIST (geom);
-CREATE INDEX IF NOT EXISTS idx_postal_offices_name ON postal_offices (name);
+CREATE INDEX IF NOT EXISTS idx_postal_offices_geom
+ON rpj.postal_offices USING GIST (geom);
+
+CREATE INDEX IF NOT EXISTS idx_postal_offices_name
+ON rpj.postal_offices (name);
