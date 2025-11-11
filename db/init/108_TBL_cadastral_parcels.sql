215e802d1f8ce926f05e51d2f4bb5d3f3a9701bf
mihovilrak <mihovil.1978@hotmail.com>
2025-11-11 21:31:43 +0100

    Changed parcel_code to varchar(20), graphical_area to numeric(12, 2)

--- db/init/108_TBL_cadastral_parcels.sql
+++ db/init/108_TBL_cadastral_parcels.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS dkp.cadastral_parcels (
     id INT PRIMARY KEY,
-    parcel_code VARCHAR(255) NOT NULL,
+    parcel_code VARCHAR(20) NOT NULL,
     cadastral_municipality_code INT NOT NULL 
         REFERENCES dkp.cadastral_municipalities(national_code),
-    graphical_area FLOAT NOT NULL,
+    graphical_area NUMERIC(12, 2) NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     geom GEOMETRY(MULTIPOLYGON, 3765) NOT NULL
 );
@@ -15,3 +15,6 @@ ON dkp.cadastral_parcels (cadastral_municipality_code);
 
 CREATE INDEX IF NOT EXISTS idx_cadastral_parcels_parcel_code 
 ON dkp.cadastral_parcels (parcel_code);
+
+CREATE INDEX IF NOT EXISTS idx_cadastral_parcels_municipality_code_parcel_code 
+ON dkp.cadastral_parcels (cadastral_municipality_code, parcel_code);
