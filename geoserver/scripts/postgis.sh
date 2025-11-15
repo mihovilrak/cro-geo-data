1da3e4ab94a28ea84396bcf342cbf16c0dfe4e37
mihovilrak <mihovil.1978@hotmail.com>
2025-11-15 19:14:04 +0100

    Removed workspace and postgis scripts

--- geoserver/scripts/postgis.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST "${GEOSERVER_URL}/geoserver/rest/workspaces/cro-geo-data/datastores" \
--H  "accept: text/html" \
--H  "content-type: application/json" \
--d @json/postgis.json
--- geoserver/scripts/workspace.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST "${GEOSERVER_URL}/geoserver/rest/workspaces?default=true" \
--H  "accept: text/html" \
--H  "content-type: application/json" \
--d @json/workspace.json
