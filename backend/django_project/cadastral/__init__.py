da74b265dac1569bb686d58f06687f49b1a80dba
mihovilrak <mihovil.1978@hotmail.com>
2025-11-19 00:16:35 +0100

    Added type hints

--- backend/django_project/cadastral/__init__.py
+++ backend/django_project/cadastral/__init__.py
@@ -2,9 +2,10 @@
 App init tweaks for optional dependencies.
 """
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.views import APIView
 
 if not hasattr(DjangoFilterBackend, "get_schema_operation_parameters"):
-    def _schema_operation_parameters(self, view):
+    def _schema_operation_parameters(self, view: APIView) -> list:
         return []
 
     DjangoFilterBackend.get_schema_operation_parameters = _schema_operation_parameters  # type: ignore[attr-defined]
